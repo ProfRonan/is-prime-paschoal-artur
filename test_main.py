0d4eb3d0c10bd10fe602989dcef1df1f12d14a2a
Ronan Pardo Soares <ronan.soares@gmail.com>
2023-04-26 14:41:58 -0300

    Atualiza README e testes

--- test_main.py
+++ test_main.py
@@ -1,68 +1,96 @@
-import builtins
-import importlib
-import io
-import sys
+"""Test file for testing the main.py file"""
 
-import pytest
-from pytest import MonkeyPatch
+import unittest
+from unittest.mock import patch
+import io # for capturing the output
+import sys # for restoring the stdout and removing the main module from the cache
+import importlib # for importing the main.py file
 
+class TestMain(unittest.TestCase):
+    """Class for testing the main.py file"""
 
-@pytest.mark.parametrize(
-    "test_input", ["0", "-1", "-2", "-10"],
-)
-def test_not_valid(monkeypatch: MonkeyPatch, test_input: list[str]):
-    mocked_input = lambda prompt="": test_input
-
-    mocked_stdout = io.StringIO()
-
-    with monkeypatch.context() as m:
-        m.setattr(builtins, "input", mocked_input)
-        m.setattr(sys, "stdout", mocked_stdout)
-
+    def setUp(self):
+        """Sets up the test environment by removing the main module from the cache"""
+        super().setUp()
         sys.modules.pop("main", None)
-        importlib.import_module(name="main", package="files")
 
-    assert "Número inválido" in mocked_stdout.getvalue().strip()
-    assert "Primo" not in mocked_stdout.getvalue().strip()
-    assert "Não primo" not in mocked_stdout.getvalue().strip()
+    @patch("builtins.input", return_value="2")
+    def test_prime_2(self, _mock_input):
+        """Testa se o programa responde primo quando a entrada é 2"""
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        importlib.import_module("main")
+        sys.stdout = sys.__stdout__
+        self.assertIn("Primo", captured_output.getvalue().strip())
+        self.assertNotIn("Não primo", captured_output.getvalue().strip())
+        self.assertNotIn("Número inválido", captured_output.getvalue().strip())
+
+    @patch("builtins.input", return_value="5")
+    def test_prime_5(self, _mock_input):
+        """Testa se o programa responde primo quando a entrada é 5"""
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        importlib.import_module("main")
+        sys.stdout = sys.__stdout__
+        self.assertIn("Primo", captured_output.getvalue().strip())
+        self.assertNotIn("Não primo", captured_output.getvalue().strip())
+        self.assertNotIn("Número inválido", captured_output.getvalue().strip())
+
+    @patch("builtins.input", return_value="71")
+    def test_prime_71(self, _mock_input):
+        """Testa se o programa responde primo quando a entrada é 71"""
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        importlib.import_module("main")
+        sys.stdout = sys.__stdout__
+        self.assertIn("Primo", captured_output.getvalue().strip())
+        self.assertNotIn("Não primo", captured_output.getvalue().strip())
+        self.assertNotIn("Número inválido", captured_output.getvalue().strip())
+
+    @patch("builtins.input", return_value="1")
+    def test_not_prime_1(self, _mock_input):
+        """Testa se o programa responde não primo quando a entrada é 1"""
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        importlib.import_module("main")
+        sys.stdout = sys.__stdout__
+        self.assertIn("Não primo", captured_output.getvalue().strip())
+        self.assertNotIn("Primo", captured_output.getvalue().strip())
+        self.assertNotIn("Número inválido", captured_output.getvalue().strip())
+
+    @patch("builtins.input", return_value="4")
+    def test_not_prime_4(self, _mock_input):
+        """Testa se o programa responde não primo quando a entrada é 4"""
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        importlib.import_module("main")
+        sys.stdout = sys.__stdout__
+        self.assertIn("Não primo", captured_output.getvalue().strip())
+        self.assertNotIn("Primo", captured_output.getvalue().strip())
+        self.assertNotIn("Número inválido", captured_output.getvalue().strip())
+
+    @patch("builtins.input", return_value="289")
+    def test_not_prime_289(self, _mock_input):
+        """Testa se o programa responde não primo quando a entrada é 289"""
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        importlib.import_module("main")
+        sys.stdout = sys.__stdout__
+        self.assertIn("Não primo", captured_output.getvalue().strip())
+        self.assertNotIn("Primo", captured_output.getvalue().strip())
+        self.assertNotIn("Número inválido", captured_output.getvalue().strip())
+
+    @patch("builtins.input", return_value="-3")
+    def test_invalid(self, _mock_input):
+        """Testa se o programa responde número inválido quando a entrada é -3"""
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        importlib.import_module("main")
+        sys.stdout = sys.__stdout__
+        self.assertIn("Número inválido", captured_output.getvalue().strip())
+        self.assertNotIn("Primo", captured_output.getvalue().strip())
+        self.assertNotIn("Não primo", captured_output.getvalue().strip())
 
 
-@pytest.mark.parametrize(
-    "test_input", ["1", "4", "6", "8", "9", "10",
-                   "12", "14", "15", "16", "18", "20"],
-)
-def test_not_prime(monkeypatch: MonkeyPatch, test_input: list[str]):
-    mocked_input = lambda prompt="": test_input
-
-    mocked_stdout = io.StringIO()
-
-    with monkeypatch.context() as m:
-        m.setattr(builtins, "input", mocked_input)
-        m.setattr(sys, "stdout", mocked_stdout)
-
-        sys.modules.pop("main", None)
-        importlib.import_module(name="main", package="files")
-
-    assert "Número inválido" not in mocked_stdout.getvalue().strip()
-    assert "Primo" not in mocked_stdout.getvalue().strip()
-    assert "Não primo" in mocked_stdout.getvalue().strip()
-
-
-@pytest.mark.parametrize(
-    "test_input", ["2", "3", "5", "7", "11", "13", "17", "19", "23", "29"],
-)
-def test_prime(monkeypatch: MonkeyPatch, test_input: list[str]):
-    mocked_input = lambda prompt="": test_input
-
-    mocked_stdout = io.StringIO()
-
-    with monkeypatch.context() as m:
-        m.setattr(builtins, "input", mocked_input)
-        m.setattr(sys, "stdout", mocked_stdout)
-
-        sys.modules.pop("main", None)
-        importlib.import_module(name="main", package="files")
-
-    assert "Número inválido" not in mocked_stdout.getvalue().strip()
-    assert "Primo" in mocked_stdout.getvalue().strip()
-    assert "Não primo" not in mocked_stdout.getvalue().strip()
+if __name__ == "__main__":
+    unittest.main()
